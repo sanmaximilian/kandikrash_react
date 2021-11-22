dcb59e655bc4144f8e2f5243b8b1b607c54b6ddc
Santiago Maximiliano <sanmaximilianb@gmail.com>
2021-11-21 23:06:53 -0300

    Version 0.0.03a

---
+++ src/lo.js
@@ -0,0 +1,8 @@
+//Hacer un programa que solicite el ingreso de 3 numeros al usuario y calcule y devuelva el promedio
+
+for i=1 to 3
+input "ingrese numero" numero
+promedio = promedio + numero
+if i = 3
+promedio = promedio / 3
+end
